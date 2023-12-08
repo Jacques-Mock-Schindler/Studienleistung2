0ea0577fff13ae49aca2520b9e36a601efc9a9ce
Jacques Mock Schindler <jacques.mock@kbw.ch>
2023-12-08 17:12:04 +0100

    Schreibe eine Lösung für eine Aufgabenstellung zur Rendite

---
+++ docs/rendite.py
@@ -0,0 +1,16 @@
+def _earnings(expenses: float, income: float) -> float:
+    """Returns the earnings."""
+    return income - expenses
+
+def return_on_investment(expenses: float, income: float, investment: float) -> float:
+    """Returns the return on investment."""
+    earnings = _earnings(expenses, income)
+    roi = earnings / investment * 100
+    print(f'Your return on investment is {roi:.2f}%.')
+    return roi
+
+def _test_return_on_investment() -> None:
+    assert return_on_investment(100, 200, 1000) == 10
+    assert return_on_investment(100, 200, 100) == 100
+    assert return_on_investment(100, 200, 200) == 50
+    print("All tests passed.")
